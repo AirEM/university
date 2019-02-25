921deeeb92e9d1b2f519f709bc99c5938014755d
maior <79055276595@ya.ru>
2019-02-26 00:05:42 +0300

    New interface.

--- Computer Graphics/lab_02/gui.py
+++ Computer Graphics/lab_02/gui.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from tkinter.ttk import *
-from tkinter import Canvas, Frame, Button, Label, Entry
 
 
 class MainFrame(Tk):
@@ -11,10 +9,25 @@ class MainFrame(Tk):
         self.width = 700
         self.height = 700
 
-        self.a = None
-        self.b = None
-        self.r = None
-        self.c = None
+        # Перенос
+
+        self.dx = None
+        self.dy = None
+
+        # Масштабирование
+
+        self.xm = None
+        self.ym = None
+        self.kx = None
+        self.ky = None
+
+        # Поворот
+
+        self.xc = None
+        self.yc = None
+        self.fi = None
+
+        # other
 
         self.display_text = None
 
@@ -44,55 +57,94 @@ class MainFrame(Tk):
         down_frame.pack(fill=X, side=BOTTOM)
 
         right_frame.pack(fill=Y, side=RIGHT)
-        self.create_paint_frame(right_frame).pack(side=TOP, pady=15)
-        # self.create_hyperbola_frame(right_frame).pack(side=TOP)
+
+        self.create_carry_frame(right_frame).pack(side=TOP, pady=15)
+        self.create_scaling_frame(right_frame).pack(side=TOP, pady=15)
+        self.create_rotation_frame(right_frame).pack(side=TOP, pady=15)
 
         self.canvas.pack(fill=BOTH, side=LEFT, expand=True)
 
-    def create_paint_frame(self, right_frame):
-        paint_frame = Frame(right_frame)
+    def create_carry_frame(self, right_frame):
+        carry_frame = Frame(right_frame)
 
-        self.a = Entry(paint_frame, width=5)
-        self.b = Entry(paint_frame, width=5)
-        self.r = Entry(paint_frame, width=5)
-        self.c = Entry(paint_frame, width=5)
+        self.dx = Entry(carry_frame, width=5)
+        self.dy = Entry(carry_frame, width=5)
 
-        rect_label = Label(paint_frame, text="Окружность\n\n(x-a)^2 + (y-b)^2 = r^2\n", anchor="center")
+        carry_label = Label(carry_frame, text="Перенос", anchor="center")
+        carry_label.grid(row=0, column=0, columnspan=4, pady=7)
 
-        rect_label.grid(row=0, column=0, columnspan=4)
+        Label(carry_frame, text="dx = ").grid(row=1, column=0, sticky=E)
+        self.dx.grid(row=1, column=1, sticky=W)
 
-        Label(paint_frame, text="a = ").grid(row=1, column=0, sticky=E)
-        self.a.grid(row=1, column=1, sticky=W)
+        Label(carry_frame, text="dy = ").grid(row=1, column=2, sticky=E)
+        self.dy.grid(row=1, column=3, sticky=W)
 
-        Label(paint_frame, text="b = ").grid(row=2, column=0, sticky=E)
-        self.b.grid(row=2, column=1, sticky=W)
+        btn_carry = Button(carry_frame, text="Выполнить")
+        btn_carry.grid(row=2, column=0, columnspan=4, ipadx=50, padx=2, pady=5)
 
-        Label(paint_frame, text="r = ").grid(row=3, column=0, sticky=E)
-        self.r.grid(row=3, column=1, sticky=W)
+        return carry_frame
 
-        point_label = Label(paint_frame, text="\n\nГипербола\n\ny = c / x", anchor="center")
-        point_label.grid(row=4, column=0, columnspan=4)
+    def create_scaling_frame(self, right_frame):
+        scaling_frame = Frame(right_frame)
 
-        Label(paint_frame, text="c = ").grid(row=5, column=0, sticky=E, pady=5)
-        self.c.grid(row=5, column=1, sticky=W, pady=2)
+        self.xm = Entry(scaling_frame, width=5)
+        self.ym = Entry(scaling_frame, width=5)
+        self.kx = Entry(scaling_frame, width=5)
+        self.ky = Entry(scaling_frame, width=5)
 
-        btn_do = Button(paint_frame, text="DO")
-        btn_do.grid(row=6, column=0, columnspan=4, ipadx=50, padx=2, pady=2)
+        point_label = Label(scaling_frame, text="Масштабирование", anchor="center")
+        point_label.grid(row=0, column=0, columnspan=4, pady=7)
 
-        btn_clean = Button(paint_frame, text="Clean")
-        btn_clean.grid(row=7, column=0, columnspan=4, ipadx=45, padx=2, pady=2)
+        # Центр масштабирования
 
-        return paint_frame
+        centre_label = Label(scaling_frame, text="Центр масштабирования", anchor="center")
+        centre_label.grid(row=1, column=0, columnspan=4)
 
-    def create_hyperbola_frame(self, right_frame):
-        hyperbola_frame = Frame(right_frame)
+        Label(scaling_frame, text="x = ").grid(row=2, column=0, sticky=E)
+        self.xm.grid(row=2, column=1, sticky=W)
 
-        self.c = Entry(hyperbola_frame, width=5)
+        Label(scaling_frame, text="y = ").grid(row=2, column=2, sticky=E)
+        self.ym.grid(row=2, column=3, sticky=W)
 
-        point_label = Label(hyperbola_frame, text="Гипербола\n\ny = c / x\n", anchor="center")
-        point_label.grid(row=0, column=0, columnspan=4)
+        # Коэф-ты масштабирования
 
-        Label(hyperbola_frame, text="c = ").grid(row=2, column=0, sticky=E)
-        self.c.grid(row=2, column=1, sticky=W)
+        coeff_label = Label(scaling_frame, text="Коэф-ты масштабирования", anchor="center")
+        coeff_label.grid(row=3, column=0, columnspan=4)
 
-        return hyperbola_frame
+        Label(scaling_frame, text="kx = ").grid(row=4, column=0, sticky=E)
+        self.kx.grid(row=4, column=1, sticky=W)
+
+        Label(scaling_frame, text="ky = ").grid(row=4, column=2, sticky=E)
+        self.ky.grid(row=4, column=3, sticky=W)
+
+        btn_carry = Button(scaling_frame, text="Выполнить")
+        btn_carry.grid(row=5, column=0, columnspan=4, ipadx=50, padx=2, pady=5)
+
+        return scaling_frame
+
+    def create_rotation_frame(self, frame):
+        rotation_frame = Frame(frame)
+
+        self.xc = Entry(rotation_frame, width=5)
+        self.yc = Entry(rotation_frame, width=5)
+        self.fi = Entry(rotation_frame, width=5)
+
+        point_label = Label(rotation_frame, text="Поворот", anchor="center")
+        point_label.grid(row=0, column=0, columnspan=4, pady=7)
+
+        centre_label = Label(rotation_frame, text="Центр поворота", anchor="center")
+        centre_label.grid(row=1, column=0, columnspan=4)
+
+        Label(rotation_frame, text="x = ").grid(row=2, column=0, sticky=E)
+        self.xc.grid(row=2, column=1, sticky=W)
+
+        Label(rotation_frame, text="y = ").grid(row=2, column=2, sticky=E)
+        self.yc.grid(row=2, column=3, sticky=W)
+
+        Label(rotation_frame, text="Угол = ").grid(row=3, column=0, columnspan=2, sticky=E)
+        self.fi.grid(row=3, column=2, sticky=W)
+
+        btn_carry = Button(rotation_frame, text="Выполнить")
+        btn_carry.grid(row=4, column=0, columnspan=4, ipadx=50, padx=2, pady=5)
+
+        return rotation_frame
